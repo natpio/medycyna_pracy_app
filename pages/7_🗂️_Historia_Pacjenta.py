ccfade211cceb47227cd44d822c90a22f559f8b6
natpio <252204917+natpio@users.noreply.github.com>
2026-03-31 13:31:29 +0200

    Update and rename 7_Historia_Pacjenta.py to 7_🗂️_Historia_Pacjenta.py

--- pages/7_🗂️_Historia_Pacjenta.py
+++ pages/7_🗂️_Historia_Pacjenta.py
@@ -1,9 +1,13 @@
 import streamlit as st
 import pandas as pd
-from db_service import get_data_as_df
 from db_service import get_data_as_df, apply_pro_style
 
+# --- KONFIGURACJA STRONY ---
 st.set_page_config(page_title="Karta Pacjenta", page_icon="🗂️", layout="wide")
+
+# --- URUCHOMIENIE STYLU PRO ---
+apply_pro_style()
+
 st.markdown("# 🗂️ Elektroniczna Dokumentacja Medyczna (EDM)")
 st.write("Wyszukaj pacjenta, aby przejrzeć pełną historię badań i wydanych orzeczeń.")
 
