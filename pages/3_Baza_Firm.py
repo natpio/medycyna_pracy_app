ca7aa74b892fc9187c69183a7453c0e44bbb7389
natpio <252204917+natpio@users.noreply.github.com>
2026-03-31 10:59:50 +0200

    Update 3_Baza_Firm.py

--- pages/3_Baza_Firm.py
+++ pages/3_Baza_Firm.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from db_service import add_company_to_db, get_data_as_df, add_stanowisko_to_db
+from db_service import get_data_as_df, apply_pro_style
 
 # --- KONFIGURACJA STRONY ---
 st.set_page_config(page_title="Baza Firm", page_icon="🏢", layout="wide")
